e6d64869ed253ad1e331e5505e535993c3ef2bae
diyanahah <147725728+diyanahah@users.noreply.github.com>
2025-06-19 13:30:31 +0400

    new update in app

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          HELLO WORLD!!!
+          HELLO WORLD!!!welcomeeee to this classs
         </p>
       </div>
       <p className="read-the-docs">
